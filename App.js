2367ed5a6340488da4cc4a859be2a010ab49ece8
yuuki <yuukikawabata.ja@gmail.com>
2023-05-05 08:58:58 +0900

    背景色変更、その他の変更

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import AppStateListener from 'react-native-appstate-listener';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import AppStateListener from "react-native-appstate-listener";
 
 const App = () => {
   const [timeNotUsed, setTimeNotUsed] = useState(0);
@@ -10,15 +10,15 @@ const App = () => {
   useEffect(() => {
     const loadState = async () => {
       try {
-        const lastUsed = await AsyncStorage.getItem('lastUsed');
-        const timeNotUsed = await AsyncStorage.getItem('timeNotUsed');
+        const lastUsed = await AsyncStorage.getItem("lastUsed");
+        const timeNotUsed = await AsyncStorage.getItem("timeNotUsed");
 
         if (lastUsed !== null && timeNotUsed !== null) {
           setLastUsed(parseInt(lastUsed, 10));
           setTimeNotUsed(parseInt(timeNotUsed, 10));
         }
       } catch (error) {
-        console.error('Error loading data', error);
+        console.error("Error loading data", error);
       }
     };
 
@@ -27,34 +27,34 @@ const App = () => {
 
   const saveState = async () => {
     try {
-      await AsyncStorage.setItem('lastUsed', lastUsed.toString());
-      await AsyncStorage.setItem('timeNotUsed', timeNotUsed.toString());
+      await AsyncStorage.setItem("lastUsed", lastUsed.toString());
+      await AsyncStorage.setItem("timeNotUsed", timeNotUsed.toString());
     } catch (error) {
-      console.error('Error saving data', error);
+      console.error("Error saving data", error);
     }
   };
 
   function handleActive() {
-    console.log('The application is now active!');
+    console.log("The application is now active!");
     const currentTime = Date.now();
     const timeElapsed = Math.floor((currentTime - lastUsed) / 1000);
     setTimeNotUsed((prevTimeNotUsed) => {
       const newValue = prevTimeNotUsed + timeElapsed;
-      AsyncStorage.setItem('timeNotUsed', newValue.toString());
+      AsyncStorage.setItem("timeNotUsed", newValue.toString());
       return newValue;
     });
     setLastUsed(currentTime);
-    AsyncStorage.setItem('lastUsed', currentTime.toString());
+    AsyncStorage.setItem("lastUsed", currentTime.toString());
   }
 
   function handleBackground() {
-    console.log('The application is now in the background!');
+    console.log("The application is now in the background!");
     setLastUsed(Date.now());
     saveState();
   }
 
   function handleInactive() {
-    console.log('The application is now inactive!');
+    console.log("The application is now inactive!");
   }
 
   function secondsToHMS(seconds) {
@@ -74,7 +74,9 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>スマホを使っていない時間:</Text>
-      <Text style={styles.time}>{hours} 時間 {minutes} 分 {remainingSeconds} 秒</Text>
+      <Text style={styles.time}>
+        {hours} 時間 {minutes} 分 {remainingSeconds} 秒
+      </Text>
       <AppStateListener
         onActive={handleActive}
         onBackground={handleBackground}
@@ -86,11 +88,9 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'black',
-    color: 'white',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 24,
