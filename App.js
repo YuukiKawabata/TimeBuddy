39a171d662dfe0fe970d5922b79e5c0e87d3b215
yuuki <yuukikawabata.ja@gmail.com>
2023-05-07 22:06:52 +0900

    画面自動オフ制御機能追加

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
 import TaskInput from "./components/TaskInput";
 import TimeDisplay from "./components/TimeDisplay";
 import ResetButton from "./components/ResetButton";
 import useTimer from "./hooks/useTimer";
 import useNotifications from "./hooks/useNotifications";
 import AppStateListener from "react-native-appstate-listener";
+import { activateKeepAwakeAsync, deactivateKeepAwakeAsync } from 'expo-keep-awake';
+
 
 const App = () => {
-  const [taskName, setTaskName] = useState("");
-  const { timeNotUsed, handleActive, setAppIsActive, resetTimeNotUsed } = useTimer();
-  useNotifications(handleActive);
+  useEffect(() => {
+    activateKeepAwakeAsync();
 
+    return () => {
+      deactivateKeepAwakeAsync();
+    };
+  }, []);
+
+  const [taskName, setTaskName] = useState("");
+  const { timeNotUsed, handleActive, setAppIsActive, resetTimeNotUsed } =
+    useTimer();
+  useNotifications(handleActive);
   return (
     <View style={styles.container}>
       <TaskInput taskName={taskName} setTaskName={setTaskName} />
@@ -24,6 +34,10 @@ const App = () => {
         }}
         onBackground={() => setAppIsActive(false)}
       />
+      {/* <KeepAwake /> */}
+      <Text style={styles.title}>
+        ※このアプリは画面がオフにならないように設定されています
+      </Text>
     </View>
   );
 };
@@ -34,6 +48,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "black",
+  },
+  title: {
+    color: "white",
+    fontSize: 10,
+    marginTop: 10,
   },
 });
 
